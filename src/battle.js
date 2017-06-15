f039f2cdab11e52f8adab72f8014788c49d8026e
mssngr <gabriel@ecliptic.io>
2017-06-15 14:04:02 -0600

    Initial code ideas.

---
+++ src/battle.js
@@ -0,0 +1,41 @@
+import readlineSync from 'readline-sync'
+import {unwen, ulfric} from '../lib/characters'
+import {calcPlayer, calcEnemy} from '../lib/tools2'
+import * as CONSTANTS from '../lib/constants'
+
+const A = CONSTANTS.COMBAT_ACTIONS[0]
+const C = CONSTANTS.COMBAT_ACTIONS[1]
+const T = CONSTANTS.COMBAT_ACTIONS[2]
+const B = CONSTANTS.COMBAT_ACTIONS[3]
+const R = CONSTANTS.COMBAT_ACTIONS[4]
+
+let player = unwen
+let enemy = ulfric
+let action
+enemy.action = A
+
+console.log(`
+You are ${player.name}.
+You hold a ${player.weapon.name}.
+You wear ${player.armor.name}
+You are engaged in battle. Prepare yourself.`)
+
+console.log(`
+Your health is ${player.health}.
+Your enemy's is ${enemy.health}.`)
+
+action = readlineSync.keyInSelect(CONSTANTS.COMBAT_ACTIONS, 'What do you choose to do?')
+player.action = CONSTANTS.COMBAT_ACTIONS[action]
+calcPlayer(player, enemy)
+
+console.log(`
+Your health is ${player.health}.
+Your enemy's is ${enemy.health}.`)
+
+action = readlineSync.keyInSelect(CONSTANTS.COMBAT_ACTIONS, 'What do you choose to do?')
+player.action = CONSTANTS.COMBAT_ACTIONS[action]
+calcPlayer(player, enemy)
+
+console.log(`
+Your health is ${player.health}.
+Your enemy's is ${enemy.health}.`)
